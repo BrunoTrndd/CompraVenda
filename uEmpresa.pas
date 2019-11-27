36d34f7b730811b26738bf3965afbe66ea2ba948
matheusmay <58177848+matheusmay@users.noreply.github.com>
2019-11-27 09:51:55 -0300

    Created uEmpresa

--- uEmpresa.pas
+++ uEmpresa.pas
@@ -2,6 +2,29 @@ unit uEmpresa;
 
 interface
 
+uses SysUtils, Generics.Collections, uOrdem, uPessoa, uEstoque;
+
+type
+  TEmpresa = class
+  private
+    FNome    : string;
+    FSaldo   : currency;
+    FOrdens  : TList<TOrdem>;
+    FPessoas : TList<TPessoa>;
+    FEstoque : TList<TEstoque>;
+
+  public
+    property Nome    : string          read FNome    write FNome;
+    property Saldo   : currency        read FSaldo   write FSaldo;
+    property Ordens  : TList<TOrdem>   read FOrdens  write FOrdens;
+    property Pessoas : TList<TPessoa>  read FPessoas write FPessoas;
+    property Estoque : TList<TEstoque> read FEstoque write FEstoque;
+
+  //CONSTRUCTOR
+  constructor TEmpresa.Create;
+
+  end;
+
 implementation
 
 end.
