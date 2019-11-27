5d240936d9f65d865727acc74c6c607b3eb66956
matheusmay <58177848+matheusmay@users.noreply.github.com>
2019-11-27 08:52:57 -0300

    Created unit uProduto

--- uProduto.pas
+++ uProduto.pas
@@ -2,6 +2,64 @@ unit uProduto;
 
 interface
 
+uses SysUtils, Generics.Collections;
+
+type
+  TProduto = class
+  private
+    FNome               : string;
+    FValorUnit          : currency;
+    FTipoProduto        : string; //TTipoProduto
+    FEmpresa            : TEmpresa;
+    FValorCompra        : currency;
+    FValorVenda         : currency;
+    FSaldoVenda         : integer;
+    FSaldoDisponivel    : integer;
+    FNaturezaMercadoria : TNaturezaMercadoria;
+
+  public
+    property Nome               : string read FNome write FNome;
+    property ValorUnit          : currency read FValorUnit write FValorUnit;
+    property TipoProduto        : string read FTipoProduto write FTipoProduto;
+    property Empresa            : TEmpresa read FEmpresa write FEmpresa;
+    property ValorCompra        : currency read FValorCompra write FValorCompra;
+    property ValorVenda         : currency read FValorVenda write FValorVenda;
+    property SaldoDisponivel    : integer read FSaldoDisponivel write FSaldoDisponivel;
+    property SaldoVenda         : integer read FSaldoVenda write FSaldoVenda;
+    property NaturezaMercadoria : TNaturezaMercadoria read FNaturezaMercadoria write FNaturezaMercadoria;
+
+  //CONSTRUCTORS
+  constructor Create();
+  destructor  destroy;override;
+
+  //PROCEDURES
+  procedure SolicitarInformacao();
+  procedure ToString();
+
+  //FUNCTIONS
+
+  end;
+
 implementation
 
+{ TProduto }
+
+
+constructor TProduto.Create;
+begin
+  FNome               := '';
+  FValorUnit          := 0.00;
+  FTipoProduto        := ''; //TTipoProduto
+  FEmpresa            := nil;
+  FValorCompra        := 0.00;
+  FValorVenda         := 0.00;
+  FSaldoVenda         := 0;
+  FSaldoDisponivel    := 0;
+  FNaturezaMercadoria := nil;
+end;
+
+destructor TProduto.Destroy;
+begin
+
+end;
 end.
